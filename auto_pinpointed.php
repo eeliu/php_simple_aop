536d414d3c72ca3d672089d74ed07d09ab3384cb
liumingyi <liumingyi@navercorp.com>
2019-06-29 15:40:12 +0800

    [06-29] add RequestOncePlugins

--- auto_pinpointed.php
+++ auto_pinpointed.php
@@ -6,3 +6,8 @@ require AUTOLOAD_FILE_ALIAS;
 use pinpoint\Common\PinpointDriver;
 
 PinpointDriver::getInstance()->init();
+$oncePlugins = null;
+//todo add TSpan plugins
+if(class_exists("\Plugins\RequestOncePlugins")){
+    $oncePlugins = new \Plugins\RequestOncePlugins();
+}
